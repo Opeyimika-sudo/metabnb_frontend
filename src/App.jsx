3c2a2426253cc33b7980d35bbf920bacadcccfd7
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-11 04:34:24 +0100

    update: import Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 import './App.css'
 import Header from '../src/components/Header'
+import Hero from '../src/components/Hero'
+
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   )
 }
