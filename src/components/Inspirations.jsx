a41d0214245ec198fbf897aa03df7f33fa4fb9d0
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-11-12 12:13:11 +0100

    feat: build the structure of the Inspirations component

--- src/components/Inspirations.jsx
+++ src/components/Inspirations.jsx
@@ -10,28 +10,43 @@ import inspiration_image_six from '../images/inspiration_image--six.png'
 import inspiration_image_seven from '../images/inspiration_image--seven.png'
 import inspiration_image_eight from '../images/inspiration_image--eight.png'
 
+// Star for ratings
+import star from '../images/star.png'
+
 export default function Inspirations() {
     const inspirationImages = [
         inspiration_image_one, inspiration_image_two,
         inspiration_image_three, inspiration_image_four, inspiration_image_five, inspiration_image_six, inspiration_image_seven, inspiration_image_eight
     ];
+    
     const Inspirations =inspirationImages.map((item) => (
-        <div className='inspirations'>
-            <h2>Inspiration for your next adventure</h2>
-            {console.log("amazing")}
-
-            <div className='inspirations_images'>
-                <div className='card'>
+            <div className='card'>
                     <img src={item} alt="" className='card_image' />
-                    <p>Desert king</p>
-                    <p>1MBT per night</p>
-                    <p>2345km away</p>
-                    <p>available for 2weeks stay</p>
-                </div>
-            </div>
+                    <div className="card_text">
+                        <p>Desert king</p>
+                        <p id="bolden">1MBT per night</p>
+                    </div>
+                    <div className="card_text">
+                        <p>2345km away</p>
+                        <p>available for 2weeks stay</p>
+                    </div>
+                    <div className='star_rating'>
+                        <img src={star} alt="" className="star" />
+                        <img src={star} alt="" className="star" />
+                        <img src={star} alt="" className="star" />
+                        <img src={star} alt="" className="star" />
+                        <img src={star} alt="" className="star" />
+                    </div>        
             </div>
     ));
   return (
-    [Inspirations]
+    <div className='inspirations'>
+        <h2 id="inspirations_text">Inspiration for your next adventure</h2>
+        <div className='cards'>
+            {Inspirations}
+        </div>
+        
+    </div>
+    
   )
 }
